49f2464b079b1c18fbbc43316673fd5a0f49501b
Admin <admin@Bills-Mac-mini-4.local>
2023-09-26 09:22:42 -0400

    basic function completed, message only

---
+++ timer.py
@@ -0,0 +1,21 @@
+"""
+This program is used as an example for MGTC28.
+timer.py is a simple Python script that will allow user to set timer duration.
+Upon timer expiry, user will see the time up meme and sound notification.
+timer.py uses the time library to help keep track of time
+"""
+
+
+# This program is timer that counts down
+
+
+import time # The time library has a sleep function that will pause the script for a specifized amount of time
+import pygame # the pygame library makes it easy to display images and make sounds
+
+# ask user to enter desired countdown time
+set_time = input("Please set your timer in seconds: ")
+
+time.sleep(set_time)
+
+snaps.display_message("Time's up!!")
+
